993a8b22a1b5857317551a8356e4e2aaba947a5b
Kanishka Ghosh <kghosh@nd.edu>
2020-12-17 11:21:50 -0500

    added function to test calculate angle function with multiple test cases using decorators

--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -30,3 +30,12 @@ def test_calculate_angle():
     calculated_angle = molecool.calculate_angle(p1,p2,p3,degrees=True)
 
     assert pytest.approx(expected_angle) == calculated_angle
+
+@pytest.mark.parametrize("p1, p2, p3, expected_angle",[
+    (np.array([np.sqrt(2)/2, np.sqrt(2)/2, 0]),np.array([0, 0, 0]),np.array([1, 0, 0]), 45),
+    (np.array([0, 0, -1]),np.array([0, 1, 0]),np.array([1, 0, 0]), 60)
+])
+def test_calculate_angle_many(p1, p2, p3, expected_angle):
+    calculated_angle = molecool.calculate_angle(p1, p2, p3, degrees=True)
+
+    assert pytest.approx(expected_angle) == calculated_angle
