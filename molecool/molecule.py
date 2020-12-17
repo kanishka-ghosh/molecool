508ca186e79b15b13773359102f9eb1a0c17480a
Kanishka Ghosh <kghosh@nd.edu>
2020-12-17 09:19:57 -0500

    added test for calculating molecular mass and updated function to calculate molecular mass

--- molecool/molecule.py
+++ molecool/molecule.py
@@ -3,6 +3,7 @@ Functions for analyzing/measuring molecules
 """
 
 from .measure import calculate_distance
+from .atom_data import atomic_weights
 
 def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
 
@@ -17,3 +18,50 @@ def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
                 bonds[(atom1, atom2)] = distance
 
     return bonds
+
+def calculate_molecular_mass(symbols):
+   """Calculate the mass of a molecule.
+
+   Parameters
+   ----------
+   symbols : list
+       A list of elements.
+
+   Returns
+   -------
+   mass : float
+       The mass of the molecule
+   """
+   mol_mass = 0.0
+   for symbol in symbols:
+       mol_mass += atomic_weights[symbol]
+
+   return mol_mass
+
+
+def calculate_center_of_mass(symbols, coordinates):
+   """Calculate the center of mass of a molecule.
+
+   The center of mass is weighted by each atom's weight.
+
+   Parameters
+   ----------
+   symbols : list
+       A list of elements for the molecule
+   coordinates : np.ndarray
+       The coordinates of the molecule.
+
+   Returns
+   -------
+   center_of_mass: np.ndarray
+       The center of mass of the molecule.
+
+   Notes
+   -----
+   The center of mass is calculated with the formula
+
+   .. math:: \\vec{R}=\\frac{1}{M} \\sum_{i=1}^{n} m_{i}\\vec{r_{}i}
+
+   """
+
+   return np.array([])
--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -7,10 +7,10 @@ import molecool
 import numpy as np
 
 def test_calculate_distance():
-    
+
     r1 = np.array([0, 0, 0])
     r2 = np.array([0, 1, 0])
-    
+
     expected_distance = 1
 
     calculated_distance = molecool.calculate_distance(r1, r2)
